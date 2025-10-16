45a568d99a14feaccb48d894de7feb52868e5e0d
malkah04 <your-email@example.com>
2025-10-16 13:59:38 +0300

    test

--- src/app/java/Main.java
+++ src/app/java/Main.java
@@ -1,10 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package app.java;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(sum(1,2));
+        System.out.println(multiply(3,4));
     }
     public static int sum(int a, int b) {
         return a + b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
     }
 }
---
+++ src/app/java/MainTest.java
@@ -0,0 +1,18 @@
+package app.java;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void testSum() {
+        int result = Main.sum(2 , 3);
+        assertEquals(6, result);
+    }
+    @Test
+    void testMultiply() {
+        int result = Main.multiply(3, 4);
+        assertEquals(12, result);
+    }
+}
